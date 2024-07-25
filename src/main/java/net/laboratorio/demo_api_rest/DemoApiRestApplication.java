7432f8111089708b8bfce6a0df52de756893c31d
cerso <cersosimovicente@gmail.com>
2024-07-25 16:38:24 -0300

    add environment

--- src/main/java/net/laboratorio/demo_api_rest/DemoApiRestApplication.java
+++ src/main/java/net/laboratorio/demo_api_rest/DemoApiRestApplication.java
@@ -1,5 +1,6 @@
 package net.laboratorio.demo_api_rest;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApiRestApplication {
 
 	public static void main(String[] args) {
+		// Cargar variables del archivo .env
+		Dotenv dotenv = Dotenv.load();
+		// Configurar propiedades del sistema
+		System.setProperty("spring.datasource.url", dotenv.get("DATABASE_URL"));
+		System.setProperty("spring.datasource.username", dotenv.get("DATABASE_USERNAME"));
+		System.setProperty("spring.datasource.password", dotenv.get("DATABASE_PASSWORD"));
 		SpringApplication.run(DemoApiRestApplication.class, args);
 	}
 
